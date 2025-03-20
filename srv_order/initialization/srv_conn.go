1a7731b6cf76246eac8024a011b135ecaca101b4
gww <gww19952018@outlook.com>
2025-03-20 20:52:53 +0800

    order_srv

---
+++ srv_order/initialization/srv_conn.go
@@ -0,0 +1,39 @@
+package initialization
+
+import (
+	"fmt"
+	"goShop/srv_order/global"
+	"goShop/srv_order/proto"
+
+	_ "github.com/mbobakov/grpc-consul-resolver" // It's important
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+)
+
+func InitSrvConn() {
+	consulInfo := global.ServerConfig.ConsulInfo
+	goodsConn, err := grpc.Dial(
+		fmt.Sprintf("consul://%s:%d/%s?wait=14s", consulInfo.Host, consulInfo.Port, global.ServerConfig.GoodsSrvInfo.Name),
+		grpc.WithInsecure(),
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`),
+		//grpc.WithUnaryInterceptor(otgrpc.OpenTracingClientInterceptor(opentracing.GlobalTracer())),
+	)
+	if err != nil {
+		zap.S().Fatal("[InitSrvConn] 连接 【商品服务失败】")
+	}
+
+	global.GoodsSrvClient = proto.NewGoodsClient(goodsConn)
+
+	//初始化库存服务连接
+	invConn, err := grpc.Dial(
+		fmt.Sprintf("consul://%s:%d/%s?wait=14s", consulInfo.Host, consulInfo.Port, global.ServerConfig.InventorySrvInfo.Name),
+		grpc.WithInsecure(),
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`),
+		//grpc.WithUnaryInterceptor(otgrpc.OpenTracingClientInterceptor(opentracing.GlobalTracer())),
+	)
+	if err != nil {
+		zap.S().Fatal("[InitSrvConn] 连接 【库存服务失败】")
+	}
+
+	global.InventorySrvClient = proto.NewInventoryClient(invConn)
+}
